293c4e601bdcb39e2a6b5c8614fe5ae3c8d7565c
Anderson Faustino Lima <afl@lionx.com.br>
2021-06-02 09:30:47 -0400

    [Persehone-Client] modfied project structure

--- persephone_client/main.py
+++ persephone_client/main.py
@@ -1,21 +1,27 @@
 from persephone_client.controllers.schema_validator.controller import SchemaValidatorController
 from persephone_client.controllers.queue_producer.controller import QueueProducerController
 from kafka import KafkaProducer
+from jsonschema import validate
 
 
 class Persephone:
 
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, port: int) -> None:
         self.producer = KafkaProducer(bootstrap_servers=f'{host}:{port}')
+        self.validator = validate
+
+    @classmethod
+    def with_dependency_injection(cls, validator: any, producer: any) -> None:
+        return cls(validator=validator, producer=producer)
 
     def run(self, topic: str, partition: int, msg: dict, schema: dict) -> bool:
 
-        if self._validate(msg=msg, schema=schema):
+        if self._validate(validator=self.validator, msg=msg, schema=schema):
             QueueProducerController.send_to_queue(producer=self.producer, topic=topic, partition=partition, msg=msg)
             return True
         else:
             return False
 
     @staticmethod
-    def _validate(msg: dict, schema: dict) -> bool:
-        return SchemaValidatorController.schema_validator(msg=msg, schema=schema)
+    def _validate(validator: any, msg: dict, schema: dict) -> bool:
+        return SchemaValidatorController.schema_validator(validator=validator, msg=msg, schema=schema)
--- tests/persephone_client/services/queue_producer/test_service.py
+++ tests/persephone_client/services/queue_producer/test_service.py
@@ -1,4 +1,4 @@
-from persephone_client.services.queue_producer.service import QueueProducerService
+from persephone_client.controllers.queue_producer.controller import QueueProducerController
 from unittest.mock import MagicMock
 import pytest
 
@@ -13,38 +13,38 @@ def test_invalid_message_as_empty_byte():
     producer.send = MagicMock(return_value=True)
     msg = b""
     with pytest.raises(Exception, match="Given message must be dict type"):
-        QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+        QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
 
 def test_invalid_message_as_empty_str():
     producer.send = MagicMock(return_value=True)
     msg = ""
     with pytest.raises(Exception, match="Given message must be dict type"):
-        QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+        QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
 
 
 def test_invalid_message_as_byte():
     producer.send = MagicMock(return_value=True)
     msg = b"teste messsage"
     with pytest.raises(Exception, match="Given message must be dict type"):
-        QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+        QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
 
 def test_invalid_message_as_str():
     producer.send = MagicMock(return_value=True)
     msg = "teste message"
     with pytest.raises(Exception, match="Given message must be dict type"):
-        QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+        QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
 
 def test_invalid_message_as_byte():
     producer.send = MagicMock(return_value=True)
     msg = b"teste messsage"
     with pytest.raises(Exception, match="Given message must be dict type"):
-        QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+        QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
 
 def test_invalid_message_as_str():
     producer.send = MagicMock(return_value=True)
     msg = "teste message"
     with pytest.raises(Exception, match="Given message must be dict type"):
-        QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+        QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
 
 
 
@@ -52,9 +52,9 @@ def test_invalid_message_as_dict_empty():
     producer.send = MagicMock(return_value=True)
     msg = {}
     with pytest.raises(Exception, match="Given message must not be empty"):
-        QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+        QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
 
 def test_invalid_message_as_dict_valid():
     producer.send = MagicMock(return_value=True)
     msg = {"key": "value"}
-    QueueProducerService.send_to_queue(producer=producer, topic="test", partition=0, msg=msg)
+    assert QueueProducerController.send_to_queue(producer=producer, topic="test", partition=0, msg=msg) is None
