4e810efb3729d2fa20e1a4e92643b417de6db9a5
Anderson Faustino Lima <afl@lionx.com.br>
2021-06-04 17:01:43 -0400

    [Persehone-Client] added builder validator (2)

--- tests/persephone_client/services/schema_validator/test_service.py
+++ tests/persephone_client/services/schema_validator/test_service.py
@@ -1,110 +1,117 @@
-from persephone_client.controllers.schema_validator.controller import SchemaValidatorController
+from persephone_client.controllers.schema_validator.controller import (
+    SchemaValidatorController,
+)
 from unittest.mock import MagicMock
 import pytest
 
 
 def test_invalid_message_as_empty_str():
     payload = ""
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_message_as_empty_byte():
     payload = b""
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_message_as_str():
     payload = "test message"
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_message_as_byte():
     payload = b"test message"
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_message_as_empty_dict():
     payload = {}
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must not be empty"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_schema_as_empty_dict():
     payload = {"key": "test"}
     schema = {}
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must not be empty"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_schema_as_empty_byte():
     payload = {"key": "test"}
     schema = b""
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_schema_as_empty_str():
     payload = {"key": "test"}
     schema = ""
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_schema_as_str():
     payload = {"key": "test"}
     schema = "test schema"
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_invalid_schema_as_byte():
     payload = {"key": "test"}
     schema = b"test schema"
     validator = MagicMock(return_value=True)
     with pytest.raises(Exception, match="Given message must be dict type"):
-        SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema)
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+
 
 def test_validate():
     payload = {"key": "test"}
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
     validator = MagicMock(return_value=True)
-    assert SchemaValidatorController.schema_validator(validator, payload=payload, schema=schema) is True
+    assert (
+        SchemaValidatorController.schema_validator(
+            validator, payload=payload, schema=schema
+        )
+        is True
+    )
--- tests/persephone_client/test_main.py
+++ tests/persephone_client/test_main.py
@@ -14,28 +14,26 @@ class StubbyValidator:
 
 def test_persephone_without_dependency_injection():
     payload = {"key": "test"}
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
 
     persephone = Persephone(host="localhost", port=9092)
-    assert persephone.run(topic="test", partition=0, payload=payload, schema=schema) is True
+    assert (
+        persephone.run(topic="test", partition=0, payload=payload, schema=schema)
+        is True
+    )
 
 
 def test_persephone_with_dependency_injection_True():
     payload = {"key": "test"}
-    schema = {
-        "type": "object",
-        "properties": {
-            "key": {"type": "string"}
-        }
-    }
+    schema = {"type": "object", "properties": {"key": {"type": "string"}}}
 
     persephone = Persephone(host="localhost", port=9092)
     producer = MagicMock(return_value=True)
     validator = MagicMock(return_value=True)
-    persephone = persephone.with_dependency_injection(validator=validator, producer=producer)
-    assert persephone.run(topic="test", partition=0, payload=payload, schema=schema) is True
+    persephone = persephone.with_dependency_injection(
+        validator=validator, producer=producer
+    )
+    assert (
+        persephone.run(topic="test", partition=0, payload=payload, schema=schema)
+        is True
+    )
