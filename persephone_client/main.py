3f61d4d5852f04b94d693de696bd3b4d0c46e4b5
Ig0or <igp@lionx.com.br>
2022-02-23 17:39:57 -0300

    update main

--- persephone_client/main.py
+++ persephone_client/main.py
@@ -42,3 +42,17 @@ class Persephone:
     @staticmethod
     def _validate(payload: dict, schema: str, logger: any) -> bool:
         return SchemaValidatorController.schema_validator(payload=payload, schema_to_use=schema, logger=logger)
+
+
+if __name__ == '__main__':
+    dic = {
+        'unique_id': 'dbfffc8d-a57e-4156-95da-bde8bbaa78d3',
+        'previous_electronic_signature': 'ea73ced01f94d96f7f46682055d6e3915b626a2ebbf98818a09ea2efb6af1a9e',
+        'previous_is_blocked_electronic_signature': False,
+        'previous_electronic_signature_wrong_attempts': 0,
+        'new_electronic_signature': 'ea73ced01f94d96f7f46682055d6e3915b626a2ebbf98818a09ea2efb6af1a9e',
+        'new_is_blocked_electronic_signature': False,
+        'new_electronic_signature_wrong_attempts': 0}
+
+
+    print(Persephone._validate(payload=dic, schema='user_change_or_reset_electronic_signature_schema', logger=''))
