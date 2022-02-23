a1836586314289880ea348dd4ea880f0a4c2b47f
Ig0or <igp@lionx.com.br>
2022-02-23 14:18:58 -0300

    change method _get_schema

--- persephone_client/services/builder_validator/service.py
+++ persephone_client/services/builder_validator/service.py
@@ -15,14 +15,16 @@ class BuilderValidator(IBuilderValidator):
             return False
 
     @classmethod
-    def _verify_exists_schema(cls, schema_to_use: str):
-        exists_schema = schema_to_use in ChooseSchema.schemas
-        return exists_schema
+    def _get_schema(cls, schema_to_use: str):
+        exists_schema = ChooseSchema.__dict__['_member_map_'].get(schema_to_use)
+        if exists_schema:
+            return exists_schema.value
 
     @classmethod
     def handle_validator(cls, payload: dict, schema_to_use: str, logger: any) -> bool:
-        if cls._verify_exists_schema(schema_to_use):
-            schema = ChooseSchema.schemas[schema_to_use]
+        if schema := cls._get_schema(schema_to_use):
             return cls._validate_payload(schema=schema, payload=payload, logger=logger)
         else:
+            if logger:
+                logger.error(f'Schema {schema_to_use} not exists.', exc_info=True)
             return False
