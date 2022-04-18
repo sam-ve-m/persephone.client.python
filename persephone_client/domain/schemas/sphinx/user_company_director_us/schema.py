4d013dfb7304d9adb39ced1470cb6498e5d9c1d4
Marco <msa@lionx.com.br>
2022-04-18 07:53:52 -0300

    gitflow-hotfix-stash: 1.2.1

--- persephone_client/domain/schemas/sphinx/user_company_director_us/schema.py
+++ persephone_client/domain/schemas/sphinx/user_company_director_us/schema.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class UserCompanyDirectorUsSchema(BaseModel):
     unique_id: str
     company_director: bool
-    company_director_from: str
+    company_director_of: Optional[str]
