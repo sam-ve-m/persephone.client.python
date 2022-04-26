3eb9162b0adfa0b4060c62a09258220de97adc2b
Marco <msa@lionx.com.br>
2022-04-26 15:43:04 -0300

    version

--- persephone_client/domain/schemas/sphinx/user_tax_residences_us/schema.py
+++ persephone_client/domain/schemas/sphinx/user_tax_residences_us/schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class UserTaxResidencesUsSchema(BaseModel):
     unique_id: str
-    tax_residences: str
+    tax_residences: Optional[dict]
