264256fb35b4082facb29432d21bb0937365906c
Samuel V M <samuel.marcal@ligainvest.com.br>
2022-10-18 13:26:22 -0300

    feat: made device_info object into a string

--- persephone_client/domain/schemas/sphinx/user_selfie/schema.py
+++ persephone_client/domain/schemas/sphinx/user_selfie/schema.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, UUID4, constr
 
 class UserSelfieSchema(BaseModel):
     unique_id: UUID4
-    device_info: dict
+    device_info: str
     file_path: constr(min_length=2)
