b403a1811b7d169bc5256021c578bc7c7eacbdc8
Enrique William Garcia e Franco <enrique.franco@ligainvest.com.br>
2022-11-11 15:43:03 -0300

    chore: new naming in the schema

--- persephone_client/domain/schemas/mapinguari/user_update/schema.py
+++ persephone_client/domain/schemas/mapinguari/user_update/schema.py
@@ -4,8 +4,8 @@ from pydantic import BaseModel
 class MapinguariUserUpdate(BaseModel):
 
     access_ip: str
-    name_employee: str
-    email_employee: str
-    to_update_client_obj: dict
-    pre_update_client_obj: dict
+    employee_name: str
+    employee_email: str
+    client_to_update_obj: dict
+    client_pre_update_obj: dict
     update_at: int  # in seconds
