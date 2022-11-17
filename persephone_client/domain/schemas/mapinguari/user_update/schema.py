153f7c0c66ccad8ef88e793cb1913d838950166a
Enrique William Garcia e Franco <enrique.franco@ligainvest.com.br>
2022-11-17 16:36:30 -0300

    chore: field name update

--- persephone_client/domain/schemas/mapinguari/user_update/schema.py
+++ persephone_client/domain/schemas/mapinguari/user_update/schema.py
@@ -6,6 +6,6 @@ class MapinguariUserUpdate(BaseModel):
     access_ip: str
     employee_name: str
     employee_email: str
-    client_to_update_obj: dict
+    client_post_update_obj: dict
     client_pre_update_obj: dict
-    update_at: int  # in seconds
+    updated_at: int  # in seconds
