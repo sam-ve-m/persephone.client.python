8bf0c35059f471f20fa2435ffcb43fd0a7c93798
Enrique William Garcia e Franco <enrique.franco@ligainvest.com.br>
2022-11-21 17:56:20 -0300

    refac: new variable names

--- persephone_client/domain/schemas/mapinguari/user_update/schema.py
+++ persephone_client/domain/schemas/mapinguari/user_update/schema.py
@@ -6,6 +6,7 @@ class MapinguariUserUpdate(BaseModel):
     access_ip: str
     employee_name: str
     employee_email: str
-    client_post_update_obj: dict
-    client_pre_update_obj: dict
+    post_update_client_obj: dict
+    pre_update_client_obj: dict
+    client_unique_id: str
     updated_at: int  # in seconds
